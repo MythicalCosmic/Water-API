6de918a527a9ba10c0c11b9ac1ebf8497d65a19e
Abrorbek <qodirjonov0854@gmail.com>
2024-11-20 17:54:34 +0500

    Users fixed error

--- Water/settings.py
+++ Water/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'Water.middleware.request_log.RequestLogMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
